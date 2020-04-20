f7a3bc8afe5ea83ad81d94c991b43179fa173cec
Ravi Prakash Singh <Ravip@cloudthing.com>
2020-04-20 10:45:23 +0530

    Updates

---
+++ precache-manifest.02131dbe39fe237d9ac756799049541f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e424bc5a10c6b1823a0c9dd8a017d60d",
+    "url": "/ems/index.html"
+  },
+  {
+    "revision": "c22cb398784f249f9eea",
+    "url": "/ems/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "797196ce56f51a5ad68b",
+    "url": "/ems/static/js/2.e6135822.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ems/static/js/2.e6135822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22cb398784f249f9eea",
+    "url": "/ems/static/js/main.4f300a37.chunk.js"
+  },
+  {
+    "revision": "7e29f69cca1c8d552e9e",
+    "url": "/ems/static/js/runtime-main.554f3b1a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d0eb8192220af25b9db6528ead4da27.js"
+  "/ems/precache-manifest.02131dbe39fe237d9ac756799049541f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ems/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
